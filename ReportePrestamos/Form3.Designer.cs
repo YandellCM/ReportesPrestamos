89330cf937dade7bbec7c41c3f0631330e86f7da
Aaron Coste <aaron031229@gmail.com>
2025-01-27 10:38:19 -0400

    Cree el formulario 3 con todos los botones y les puse sus nombres

---
+++ ReportePrestamos/Form3.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ReportePrestamos
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.btnCliente = new System.Windows.Forms.Button();
+            this.btnPrestamo = new System.Windows.Forms.Button();
+            this.btnAmortizacion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCliente
+            // 
+            this.btnCliente.BackColor = System.Drawing.Color.Yellow;
+            this.btnCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCliente.Location = new System.Drawing.Point(260, 275);
+            this.btnCliente.Name = "btnCliente";
+            this.btnCliente.Size = new System.Drawing.Size(159, 51);
+            this.btnCliente.TabIndex = 0;
+            this.btnCliente.Text = "Cliente";
+            this.btnCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnPrestamo
+            // 
+            this.btnPrestamo.BackColor = System.Drawing.Color.Yellow;
+            this.btnPrestamo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrestamo.Location = new System.Drawing.Point(456, 275);
+            this.btnPrestamo.Name = "btnPrestamo";
+            this.btnPrestamo.Size = new System.Drawing.Size(163, 51);
+            this.btnPrestamo.TabIndex = 1;
+            this.btnPrestamo.Text = "Prestamo";
+            this.btnPrestamo.UseVisualStyleBackColor = false;
+            // 
+            // btnAmortizacion
+            // 
+            this.btnAmortizacion.BackColor = System.Drawing.Color.Yellow;
+            this.btnAmortizacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAmortizacion.Location = new System.Drawing.Point(653, 275);
+            this.btnAmortizacion.Name = "btnAmortizacion";
+            this.btnAmortizacion.Size = new System.Drawing.Size(149, 51);
+            this.btnAmortizacion.TabIndex = 2;
+            this.btnAmortizacion.Text = "Amortizacion";
+            this.btnAmortizacion.UseVisualStyleBackColor = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1048, 593);
+            this.Controls.Add(this.btnAmortizacion);
+            this.Controls.Add(this.btnPrestamo);
+            this.Controls.Add(this.btnCliente);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCliente;
+        private System.Windows.Forms.Button btnPrestamo;
+        private System.Windows.Forms.Button btnAmortizacion;
+    }
+}
