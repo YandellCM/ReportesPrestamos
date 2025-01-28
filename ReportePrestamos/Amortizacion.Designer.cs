526dadb308117faec7f54f616d95eb28af872229
Aaron Coste <aaron031229@gmail.com>
2025-01-28 08:21:17 -0400

    Les puse los nombres a cada text box de la Amortizcion

--- ReportePrestamos/Amortizacion.Designer.cs
+++ ReportePrestamos/Amortizacion.Designer.cs
@@ -37,14 +37,14 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.txtNumPago = new System.Windows.Forms.TextBox();
+            this.txtFechaPago = new System.Windows.Forms.TextBox();
+            this.txtPagoTotal = new System.Windows.Forms.TextBox();
+            this.txtIntereses = new System.Windows.Forms.TextBox();
+            this.txtAmortizacion = new System.Windows.Forms.TextBox();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.txtTasaInteres = new System.Windows.Forms.TextBox();
+            this.txtTipoAmortizacion = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -119,61 +119,61 @@
             this.label8.TabIndex = 7;
             this.label8.Text = "Tipo de amortización";
             // 
-            // textBox1
+            // txtNumPago
             // 
-            this.textBox1.Location = new System.Drawing.Point(523, 161);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(430, 22);
-            this.textBox1.TabIndex = 8;
+            this.txtNumPago.Location = new System.Drawing.Point(523, 161);
+            this.txtNumPago.Name = "txtNumPago";
+            this.txtNumPago.Size = new System.Drawing.Size(430, 22);
+            this.txtNumPago.TabIndex = 8;
             // 
-            // textBox2
+            // txtFechaPago
             // 
-            this.textBox2.Location = new System.Drawing.Point(523, 202);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(430, 22);
-            this.textBox2.TabIndex = 9;
+            this.txtFechaPago.Location = new System.Drawing.Point(523, 202);
+            this.txtFechaPago.Name = "txtFechaPago";
+            this.txtFechaPago.Size = new System.Drawing.Size(430, 22);
+            this.txtFechaPago.TabIndex = 9;
             // 
-            // textBox3
+            // txtPagoTotal
             // 
-            this.textBox3.Location = new System.Drawing.Point(523, 237);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(430, 22);
-            this.textBox3.TabIndex = 10;
+            this.txtPagoTotal.Location = new System.Drawing.Point(523, 237);
+            this.txtPagoTotal.Name = "txtPagoTotal";
+            this.txtPagoTotal.Size = new System.Drawing.Size(430, 22);
+            this.txtPagoTotal.TabIndex = 10;
             // 
-            // textBox4
+            // txtIntereses
             // 
-            this.textBox4.Location = new System.Drawing.Point(523, 273);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(430, 22);
-            this.textBox4.TabIndex = 11;
+            this.txtIntereses.Location = new System.Drawing.Point(523, 273);
+            this.txtIntereses.Name = "txtIntereses";
+            this.txtIntereses.Size = new System.Drawing.Size(430, 22);
+            this.txtIntereses.TabIndex = 11;
             // 
-            // textBox5
+            // txtAmortizacion
             // 
-            this.textBox5.Location = new System.Drawing.Point(557, 315);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(396, 22);
-            this.textBox5.TabIndex = 12;
+            this.txtAmortizacion.Location = new System.Drawing.Point(557, 315);
+            this.txtAmortizacion.Name = "txtAmortizacion";
+            this.txtAmortizacion.Size = new System.Drawing.Size(396, 22);
+            this.txtAmortizacion.TabIndex = 12;
             // 
-            // textBox6
+            // txtSaldo
             // 
-            this.textBox6.Location = new System.Drawing.Point(523, 352);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(430, 22);
-            this.textBox6.TabIndex = 13;
+            this.txtSaldo.Location = new System.Drawing.Point(523, 352);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.Size = new System.Drawing.Size(430, 22);
+            this.txtSaldo.TabIndex = 13;
             // 
-            // textBox7
+            // txtTasaInteres
             // 
-            this.textBox7.Location = new System.Drawing.Point(523, 390);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(430, 22);
-            this.textBox7.TabIndex = 14;
+            this.txtTasaInteres.Location = new System.Drawing.Point(523, 390);
+            this.txtTasaInteres.Name = "txtTasaInteres";
+            this.txtTasaInteres.Size = new System.Drawing.Size(430, 22);
+            this.txtTasaInteres.TabIndex = 14;
             // 
-            // textBox8
+            // txtTipoAmortizacion
             // 
-            this.textBox8.Location = new System.Drawing.Point(541, 427);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(412, 22);
-            this.textBox8.TabIndex = 15;
+            this.txtTipoAmortizacion.Location = new System.Drawing.Point(541, 427);
+            this.txtTipoAmortizacion.Name = "txtTipoAmortizacion";
+            this.txtTipoAmortizacion.Size = new System.Drawing.Size(412, 22);
+            this.txtTipoAmortizacion.TabIndex = 15;
             // 
             // Amortizacion
             // 
@@ -182,14 +182,14 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1255, 706);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtTipoAmortizacion);
+            this.Controls.Add(this.txtTasaInteres);
+            this.Controls.Add(this.txtSaldo);
+            this.Controls.Add(this.txtAmortizacion);
+            this.Controls.Add(this.txtIntereses);
+            this.Controls.Add(this.txtPagoTotal);
+            this.Controls.Add(this.txtFechaPago);
+            this.Controls.Add(this.txtNumPago);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -215,13 +215,13 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtNumPago;
+        private System.Windows.Forms.TextBox txtFechaPago;
+        private System.Windows.Forms.TextBox txtPagoTotal;
+        private System.Windows.Forms.TextBox txtIntereses;
+        private System.Windows.Forms.TextBox txtAmortizacion;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.TextBox txtTasaInteres;
+        private System.Windows.Forms.TextBox txtTipoAmortizacion;
     }
 }
