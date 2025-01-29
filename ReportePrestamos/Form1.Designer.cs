075a6b9a54e6e05a899973da9dad0e70fb52807a
ANGEL YANDEL CUEVAS MORA <20200817901@cafam.edu.do>
2025-01-29 09:29:52 -0500

    Registrar Prestamo Cliente Yandell

--- ReportePrestamos/Form1.Designer.cs
+++ ReportePrestamos/Form1.Designer.cs
@@ -111,7 +111,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- ReportePrestamos/Form2.Designer.cs
+++ ReportePrestamos/Form2.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.btnRegistrar = new System.Windows.Forms.Button();
             this.btnReportes = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnRegistrar
@@ -56,6 +57,17 @@
             this.btnReportes.Text = "Reportes";
             this.btnReportes.UseVisualStyleBackColor = false;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(375, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 39);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Opciones";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -63,12 +75,14 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnReportes);
             this.Controls.Add(this.btnRegistrar);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,5 +90,6 @@
 
         private System.Windows.Forms.Button btnRegistrar;
         private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Label label6;
     }
 }
