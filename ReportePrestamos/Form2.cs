6f7fa1e6814490d6176d297f103c54bd37857629
ANGEL YANDEL CUEVAS MORA <20200817901@cafam.edu.do>
2025-02-04 09:24:53 -0500

    Cambios en algunos forms Yandell

--- ReportePrestamos/Form2.cs
+++ ReportePrestamos/Form2.cs
@@ -23,5 +23,10 @@ namespace ReportePrestamos
             RegistPrestCliente f = new RegistPrestCliente();
             f.ShowDialog();
         }
+
+        private void btnReportes_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
