69565514290c0a4cc33135f8c485893785900775
Davidmixto20 <davidaniel0508@gmail.com>
2025-02-06 13:25:44 -0400

    agregando mis clases

---
+++ ReportePrestamos/MiGaleria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportePrestamos
+{
+    internal class MiGaleria
+    {
+        public class DatosObra
+        {
+            public string No_Factura { get; set; }
+            public string Titulo { get; set; }
+            public string Artista_Apellido { get; set; }
+            public string Artista_Nombre { get; set; }
+            public decimal Precio { get; set; }
+            public decimal Impuesto { get; set; }
+            public decimal Pago_Total { get; set; }
+            public string Firma_Vendedor { get; set; }
+            public DateTime Fecha { get; set; }
+        }
+    }
+}
